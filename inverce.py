be20e359a5267c960d353c338c26e32db34eac60
Hana Ghorbel <88244796+haghob@users.noreply.github.com>
2025-02-20 04:59:05 +0100

    Add files via upload

---
+++ inverce.py
@@ -0,0 +1,11 @@
+def inv(u):
+    if u < (1/3):
+        z = 3*u
+    elif u < (2/3):
+        z = 1
+    else:
+        z = 3*u-1
+    return (z)
+import random
+n= random.uniform(1,0)
+print(inv(n))
